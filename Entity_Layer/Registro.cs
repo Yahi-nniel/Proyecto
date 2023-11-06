0760fd9af915f0804d645ed96bae67ae3d2abc18
Yahinniel Vásquez <Yahinniel Vsquez@MSI>
2023-11-05 23:40:49 -0500

    Agregar archivos de proyecto.

---
+++ Entity_Layer/Registro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer
+{
+    public class Registro
+    {
+        string nombre { get; set; }
+        string apellido{ get; set; }
+        string correo { get; set; }
+        string telefono { get; set;}
+        string cedula { get; set; } 
+        string sexo { get; set; }
+        string edad { get; set; }
+    }
+}
